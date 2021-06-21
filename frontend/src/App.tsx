fc3384be0390d479ae85d4136c03bb3420ffa726
Makoto Maekawa <makoto@MacBook-Pro.local>
2021-06-21 14:29:13 +0900

    switch page react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,23 @@
 import './App.css';
 import MainCard from './component/maincard';
 import PrimarySearchAppBar from './component/appbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-      <PrimarySearchAppBar></PrimarySearchAppBar>
-      <MainCard></MainCard>
+      <Router>
+        <PrimarySearchAppBar></PrimarySearchAppBar>
+        <Switch>
+          <Route path='/hoge' component={MainCard} />
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
---
+++ frontend/src/component/sidemenu.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { useHistory, Link } from 'react-router-dom';
+
+type Anchor = 'top' | 'left' | 'bottom' | 'right';
+
+const useStyles = makeStyles({
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
+});
+
+export default function TemporaryDrawer() {
+    
+    const classes = useStyles();
+
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    
+    const anchor: Anchor = 'left';
+
+    const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+        
+        if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
+            return;
+        }
+    
+        setState({ ...state, [anchor]: open });
+    };
+
+    let history = useHistory();
+    
+    const handleLink = (path: string): void => {
+        console.log(path);
+        toggleDrawer(false);
+        history.push(path);
+    }
+
+    const sideMenuList = (anchor: Anchor) => (
+        <div
+            className={clsx(classes.list, {
+                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+            })}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text} onClick={() => handleLink('/hoge')}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                    
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text} onClick={() => handleLink('/')}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    return (
+        <div>
+            <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={toggleDrawer(true)}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(false)}>
+                    {sideMenuList(anchor)}
+            </Drawer>
+        </div>
+    );
+}
