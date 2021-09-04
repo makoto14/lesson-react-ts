2c912bc3d8e8d1dcc649ed062f5debe64ec9a7c3
makoto14 <makoto.sasuke@gmail.com>
2021-09-04 15:50:45 +0900

    add express static

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,21 @@
 import express from 'express';
+import path from 'path';
 const app: express.Express = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.send('Hello World!');
-})
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// app.get('/', (req: express.Request, res: express.Response) => {
+//     res.send('Hello World!');
+// })
 
 app.get('/api', (req: express.Request, res: express.Response) => {
-    setTimeout(() => {
-        res.json({ message: "Hello World!aaa"});
-    }, 1000)
-})
+    res.json({ message: "Hello World!aaa"});
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`listening on *:${port}`);
